126acc404182f3d469b533b08b4e66a312dce42f
Nyx <fauzi.dohc@yahoo.co.id>
2021-04-22 23:38:16 +0700

    update heroku

---
+++ src/parts/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from 'elements/Button'
+
+export default function Header(props) {
+    return (
+        <header className="spacing-sm">
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <BrandIcon />
+                    
+                </nav>
+                <div>
+                    <ul>
+                        <li>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    )
+}
