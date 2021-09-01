74a346631029ebb27259a8311c52bdb9e32e0ea6
Nyx <fauzi.dohc@yahoo.co.id>
2021-09-01 10:41:40 +0700

    ench

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,38 +1,38 @@
 import React from 'react'
 import Button from 'elements/Button'
-import BrandIcon from 'parts/IconText';
+import BrandIcon from 'parts/IconText'
 
 export default function Header(props) {
 
     const getNavLinkClass = path => {
-        return props.location.pathname === path ? "active" : "";
+        return props.location.pathname === path ? " active" : "";
     };
 
     return (
         <header className="spacing-sm">
             <div className="container">
                 <nav className="navbar navbar-expand-lg navbar-light">
-                    <BrandIcon/>
-                    <div className="collapse  navbar-collapse">
+                    <BrandIcon></BrandIcon>
+                    <div className="collapse navbar-collapse">
                         <ul className="navbar-nav ml-auto">
-                            <li className={'nav-item${getNavLinkClass("/")}'}>
+                            <li className={`nav-item${getNavLinkClass("/")}`}>
                                 <Button className="nav-link" type="link" href="/">
-                                    Home                                
+                                    Home
                                 </Button>
                             </li>
-                            <li className={'nav-item${getNavLinkClass("/browse-by")}'}>
+                            <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
                                 <Button className="nav-link" type="link" href="/browse-by">
-                                    Browse By                                
+                                    Browse By
                                 </Button>
                             </li>
-                            <li className={'nav-item${getNavLinkClass("/stories")}'}>
+                            <li className={`nav-item${getNavLinkClass("/stories")}`}>
                                 <Button className="nav-link" type="link" href="/stories">
-                                    Stories                                
+                                    Stories
                                 </Button>
                             </li>
-                            <li className={'nav-item${getNavLinkClass("/agents")}'}>
+                            <li className={`nav-item${getNavLinkClass("/agents")}`}>
                                 <Button className="nav-link" type="link" href="/agents">
-                                    Agents                                
+                                    Agents
                                 </Button>
                             </li>
                         </ul>
