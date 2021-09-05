15586063981f53f10a0d9c09182dc64b8a45d936
Nyx <fauzi.dohc@yahoo.co.id>
2021-09-05 22:45:20 +0700

    ench
    update hero

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import landingPage from "json/landingPage";
 
 import Header from "parts/Header";
+
+import Hero from "parts/Hero";
+
 export default class LandingPage extends Component {
-    render() {
-        return (
-        <>
-            <Header {...this.props}></Header>   
-        </>
-        );
-    }
+  render() {
+    console.log(this.props);
+    return (
+      <>
+        <Header {...this.props}></Header>
+        <Hero data={landingPage.hero}></Hero>
+      </>
+    );
+  }
 }
